f543dd8a1af97ddcede869b10e2dbff1f325896b
Tomasz Paterka <tomekpaterka@gmail.com>
2018-03-13 19:46:39 +0100

    Modified index.html and scrpit.js to create table

--- js/script.js
+++ js/script.js
@@ -16,10 +16,18 @@ function searchCountries() {
 function showCountriesList(resp) {
 	countriesList.empty();
 	resp.forEach(function(item) {
-		$('<li>').text('Country: ' + item.name).appendTo(countriesList);
+		var table = $('<table border=1>');
+		table.append('<tr><td>Country: </td><td>'+item.name+'</td></tr>');
+		table.append('<tr><td>Capital: </td><td>'+item.capital+'</td></tr>');
+		table.append('<tr><td>Region: </td><td>'+item.region+'</td></tr>');
+		table.append('<tr><td>Area: </td><td>'+item.area+'</td></tr>');
+		table.append('<tr><td>Population: </td><td>'+item.population+'</td></tr>');
+		/*$('<li>').text('Country: ' + item.name).appendTo(countriesList);
 		$('<li>').text('Capital: ' + item.capital).appendTo(countriesList);
 		$('<li>').text('Region: ' + item.region).appendTo(countriesList);
 		$('<li>').text('Area: ' + item.area).appendTo(countriesList);
-		$('<li>').text('Population: ' + item.population + '').appendTo(countriesList);
+		$('<li>').text('Population: ' + item.population + '').appendTo(countriesList);*/
+		
+		table.appendTo(countriesList);
 	});
 }
